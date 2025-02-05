6ab69990570c7485b177bc1013958720c5ccc56d
kristian-green-byu <kgreen29@byu.edu>
2025-02-04 21:48:32 -0700

    made orderRouter not depend on prior database

--- test/routes/orderRouter.test.js
+++ test/routes/orderRouter.test.js
@@ -3,6 +3,7 @@ const app = require('../../src/service');
 const { Role, DB } = require('../../src/database/database.js');
 
 var userRes;
+var addRes;
 var adminRes;
 var franchiseRes;
 var storeRes;
@@ -47,47 +48,23 @@ beforeAll(async () => {
     storeRes = await request(app).post(`/api/franchise/${franchiseRes.body.id}/store`).set("Authorization", `Bearer ${adminRes.body.token}`).set('Content-Type', 'application/json').send(storeRequest);
     const user = await createNormalUser();
     userRes = await request(app).put('/api/auth').send(user);
-    const orderReq = { franchiseId: franchiseRes.body.id, storeId: storeRes.body.id, items: [{ menuId: 1, description: "Veggie", price: 0.0038 }] }
+    const addReq = { title: "test pizza", description: "Boring test pizza made of cardboard", image: "pizza.png", "price": 0.0001 };
+    addRes = await request(app).put('/api/order/menu').set("Authorization", `Bearer ${adminRes.body.token}`).send(addReq);
+    const orderReq = { franchiseId: franchiseRes.body.id, storeId: storeRes.body.id, items: [{ menuId: 1, description: "test pizza", price: 0.0001 }] }
     orderRes = await request(app).post('/api/order').set('Content-Type', 'application/json').set('Authorization', `Bearer ${userRes.body.token}`).send(orderReq);
+});
+
+test("Add an item to the menu", async () => {
+    expect(addRes.status).toBe(200);
+    menuId = addRes.body[addRes.body.length - 1].id
 });
 
 test("Get the pizza menu", async () => {
     const res = await request(app).get('/api/order/menu');
     expect(res.status).toBe(200);
-    expect(res.body[0]).toEqual({
-        id: 1,
-        title: 'Veggie',
-        image: 'pizza1.png',
-        price: 0.0038,
-        description: 'A garden of delight'
-    });
-    expect(res.body[1]).toEqual({
-        id: 2,
-        title: 'Pepperoni',
-        image: 'pizza2.png',
-        price: 0.0042,
-        description: 'Spicy treat'
-    });
-    expect(res.body[2]).toEqual({
-        id: 3,
-        title: 'Margarita',
-        image: 'pizza3.png',
-        price: 0.0042,
-        description: 'Essential classic'
-    });
-    expect(res.body[3]).toEqual({
-        id: 4,
-        title: 'Crusty',
-        image: 'pizza4.png',
-        price: 0.0028,
-        description: 'A dry mouthed favorite'
-    });
-    expect(res.body[4]).toEqual({
-        id: 5,
-        title: 'Charred Leopard',
-        image: 'pizza5.png',
-        price: 0.0099,
-        description: 'For those with a darker side'
+    expect(res.body[res.body.length - 1]).toEqual({
+        id: addRes.body[addRes.body.length -1].id,
+        title: "test pizza", description: "Boring test pizza made of cardboard", image: "pizza.png", "price": 0.0001
     });
 });
 
@@ -95,7 +72,7 @@ test("Create a order for the authenticated user", async () => {
     expect(orderRes.status).toBe(200);
     expect(orderRes.body.order.franchiseId).toEqual(franchiseRes.body.id);
     expect(orderRes.body.order.storeId).toEqual(storeRes.body.id);
-    expect(orderRes.body.order.items[0]).toEqual({ menuId: 1, description: "Veggie", price: 0.0038 });
+    expect(orderRes.body.order.items[0]).toEqual({ menuId: 1, description: "test pizza", price: 0.0001 });
     expect(orderRes.body.jwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
     expect(typeof orderRes.body.order.id).toBe('number');
 });
@@ -109,20 +86,13 @@ test("Get the orders for the authenticated user", async () => {
     expect(res.body.orders[0].franchiseId).toEqual(franchiseRes.body.id);
     expect(res.body.orders[0].storeId).toEqual(storeRes.body.id);
     expect(res.body.orders[0].date).toMatch(/\d{4}-\d{2}-\S*/);
-    expect(res.body.orders[0].items[0]).toEqual({ menuId: 1, description: "Veggie", price: 0.0038, id:  orderRes.body.order.id});
+    expect(res.body.orders[0].items[0]).toEqual({ menuId: 1, description: "test pizza", price: 0.0001, id: orderRes.body.order.id });
 
     expect(res.body.page).toBe(1);
 });
 
-test("Add an item to the menu", async () => {
-    const addReq = { title:"test pizza", description: "Boring test pizza made of cardboard", image:"pizza.png", "price": 0.0001 };
-    const res = await request(app).put('/api/order/menu').set("Authorization", `Bearer ${adminRes.body.token}`).send(addReq);
-    expect(res.status).toBe(200);
-    menuId = res.body[res.body.length - 1].id
-});
-
 afterAll(async () => {
-    if(typeof menuId == 'number'){
+    if (typeof menuId == 'number') {
         await DB.removeMenuItem(menuId);
     }
 });
